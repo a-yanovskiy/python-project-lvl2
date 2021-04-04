2e7d9d8ce1451d17416cb2e49ec4f88663b3abea
a-yanovskiy <a.e.yanovskiy@gmail.com>
2021-04-04 22:10:43 +0300

    add -format flag

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,10 +4,13 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Generate diff')
 
+parser.add_argument('-f', '--format')
 parser.add_argument('first_file', type=str, help='first_file')
 parser.add_argument('second_file', type=str, help='second_file')
 
+
 args = parser.parse_args()
+parser.parse_args(['--format', 'FORMAT'])
 
 
 def main():
