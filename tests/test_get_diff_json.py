085b6a559bcc48e823843ce7f00e711758a059e2
a-yanovskiy <a.e.yanovskiy@gmail.com>
2021-07-02 15:54:33 +0300

    add tests

--- tests/test_get_diff_json.py
+++ tests/test_get_diff_json.py
@@ -5,7 +5,7 @@ from gendiff.generate_diff import extract_and_sort_all_keys as all_keys
 
 PATH_TO_JSON_1_FILE = 'tests/fixtures/json_1_test_file.json'
 PATH_TO_JSON_2_FILE = 'tests/fixtures/json_2_test_file.json'
-PATH_TO_JSON_RESULT_FILE = 'tests/fixtures/json_result_1-2_files.txt'
+PATH_TO_JSON_RESULT_1_2_FILES = 'tests/fixtures/json_result_1-2_files.txt'
 
 
 def test_get_diff_json_1_2_files():
@@ -23,7 +23,7 @@ def test_get_diff_json_1_2_files():
         all_keys(path_1, path_2)
     )
 
-    f = open(PATH_TO_JSON_RESULT_FILE)
+    f = open(PATH_TO_JSON_RESULT_1_2_FILES)
 
     result_from_json_file = f.read()
 
@@ -32,7 +32,7 @@ def test_get_diff_json_1_2_files():
 
 PATH_TO_JSON_3_FILE = 'tests/fixtures/json_3_test_file.json'
 PATH_TO_JSON_4_FILE = 'tests/fixtures/json_4_test_file.json'
-PATH_TO_JSON_RESULT_FILE = 'tests/fixtures/json_result_3-4_files.txt'
+PATH_TO_JSON_RESULT_3_4_FILES = 'tests/fixtures/json_result_3-4_files.txt'
 
 
 def test_get_diff_json_3_4_files():
@@ -50,7 +50,7 @@ def test_get_diff_json_3_4_files():
         all_keys(path_3, path_4)
     )
 
-    f = open(PATH_TO_JSON_RESULT_FILE)
+    f = open(PATH_TO_JSON_RESULT_3_4_FILES)
 
     result_from_json_file = f.read()
 
--- tests/test_get_diff_yaml.py
+++ tests/test_get_diff_yaml.py
@@ -5,10 +5,10 @@ from gendiff.generate_diff import extract_and_sort_all_keys as all_keys
 
 PATH_TO_YAML_1_FILE = 'tests/fixtures/yaml_1_test_file.yaml'
 PATH_TO_YAML_2_FILE = 'tests/fixtures/yaml_2_test_file.yaml'
-PATH_TO_YAML_RESULT_FILE = 'tests/fixtures/yaml_result_file.txt'
+PATH_TO_YAML_RESULT_1_2_FILES = 'tests/fixtures/yaml_result_1-2_files.txt'
 
 
-def test_get_diff_yaml():
+def test_get_diff_yaml_1_2_files():
     files = open_files(
         PATH_TO_YAML_1_FILE,
         PATH_TO_YAML_2_FILE
@@ -23,9 +23,35 @@ def test_get_diff_yaml():
         all_keys(path_1, path_2)
     )
 
-    f = open(PATH_TO_YAML_RESULT_FILE)
+    f = open(PATH_TO_YAML_RESULT_1_2_FILES)
 
     result_from_yaml_file = f.read()
-    # result_from_yaml_file = yaml.load(open(PATH_TO_YAML_RESULT_FILE))
+
+    assert diff_result == result_from_yaml_file
+
+
+PATH_TO_YAML_3_FILE = 'tests/fixtures/yaml_3_test_file.yaml'
+PATH_TO_YAML_4_FILE = 'tests/fixtures/yaml_4_test_file.yaml'
+PATH_TO_YAML_RESULT_3_4_FILES = 'tests/fixtures/yaml_result_3-4_files.txt'
+
+
+def test_get_diff_yaml_3_4_files():
+    files = open_files(
+        PATH_TO_YAML_3_FILE,
+        PATH_TO_YAML_4_FILE
+    )
+
+    path_3 = files[0]
+    path_4 = files[1]
+
+    diff_result = get_diff_yaml(
+        path_3,
+        path_4,
+        all_keys(path_3, path_4)
+    )
+
+    f = open(PATH_TO_YAML_RESULT_3_4_FILES)
+
+    result_from_yaml_file = f.read()
 
     assert diff_result == result_from_yaml_file
