71ce09c16793d14bf7525b043f9a42f85714b9ed
a-yanovskiy <a.e.yanovskiy@gmail.com>
2021-11-13 14:17:34 +0300

    rename tests

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,24 +5,24 @@ from gendiff.generate_diff import generate_diff
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Generate diff')
+    parser = argparse.ArgumentParser(description="Generate diff")
 
-    parser.add_argument('-f', '--format')
-    parser.add_argument('first_file', type=str, help='first_file')
-    parser.add_argument('second_file', type=str, help='second_file')
+    parser.add_argument("-f", "--format")
+    parser.add_argument("first_file", type=str, help="first_file")
+    parser.add_argument("second_file", type=str, help="second_file")
 
     args = parser.parse_args()
     args_dict = vars(args)
-    first_file = args_dict['first_file']
-    second_file = args_dict['second_file']
+    first_file = args_dict["first_file"]
+    second_file = args_dict["second_file"]
 
     return first_file, second_file
 
 
 def main():
     first_file, second_file = parse_arguments()
-    generate_diff(first_file, second_file)
+    print(generate_diff(first_file, second_file))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
