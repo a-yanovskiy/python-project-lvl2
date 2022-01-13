71fb00176deb10648e866d6e363799c760123a34
a-yanovskiy <a.e.yanovskiy@gmail.com>
2022-01-13 19:34:00 +0300

    fix test make diff

--- tests/test_make_diff.py
+++ tests/test_make_diff.py
@@ -2,6 +2,21 @@ from gendiff.open_files import open_files
 from gendiff.make_diff import make_diff
 
 
+PATH_TO_JSON_1_FILE = "tests/fixtures/json_1_test_file.json"
+PATH_TO_JSON_2_FILE = "tests/fixtures/json_2_test_file.json"
+PATH_TO_JSON_3_FILE = "tests/fixtures/json_3_test_file.json"
+PATH_TO_JSON_4_FILE = "tests/fixtures/json_4_test_file.json"
+PATH_TO_JSON_FIRST_RESULT = "tests/fixtures/json_diff_first_result.txt"
+PATH_TO_JSON_SECOND_RESULT = "tests/fixtures/json_diff_second_result.txt"
+
+PATH_TO_YAML_1_FILE = "tests/fixtures/yaml_1_test_file.yaml"
+PATH_TO_YAML_2_FILE = "tests/fixtures/yaml_2_test_file.yaml"
+PATH_TO_YAML_3_FILE = "tests/fixtures/yaml_3_test_file.yaml"
+PATH_TO_YAML_4_FILE = "tests/fixtures/yaml_4_test_file.yaml"
+PATH_TO_YAML_FIRST_RESULT = "tests/fixtures/yaml_diff_first_result.txt"
+PATH_TO_YAML_SECOND_RESULT = "tests/fixtures/yaml_diff_second_result.txt"
+
+
 def check_make_diff(path_1, path_2, result):
     files = open_files(
         path_1,
@@ -13,10 +28,6 @@ def check_make_diff(path_1, path_2, result):
 
     diff_result = str(make_diff(file1, file2))
 
-    # result_file = open(result)
-    # result_from_file = result_file.read()
-
-
     with open(result, 'r') as file:
         result_from_file = file.read().replace('\n', '')
 
@@ -25,28 +36,20 @@ def check_make_diff(path_1, path_2, result):
 
 def test_make_diff_json_1():
 
-    PATH_TO_1_FILE = "tests/fixtures/json_1_test_file.json"
-    PATH_TO_2_FILE = "tests/fixtures/json_2_test_file.json"
-    PATH_TO_FIRST_RESULT = "tests/fixtures/json_diff_first_result.txt"
-
     result = check_make_diff(
-        PATH_TO_1_FILE,
-        PATH_TO_2_FILE,
-        PATH_TO_FIRST_RESULT,
+        PATH_TO_JSON_1_FILE,
+        PATH_TO_JSON_2_FILE,
+        PATH_TO_JSON_FIRST_RESULT,
     )
     assert result[0] == result[1]
 
 
 def test_make_diff_json_2():
 
-    PATH_TO_3_FILE = "tests/fixtures/json_3_test_file.json"
-    PATH_TO_4_FILE = "tests/fixtures/json_4_test_file.json"
-    PATH_TO_SECOND_RESULT = "tests/fixtures/json_diff_second_result.txt"
-
     result = check_make_diff(
-        PATH_TO_3_FILE,
-        PATH_TO_4_FILE,
-        PATH_TO_SECOND_RESULT,
+        PATH_TO_JSON_3_FILE,
+        PATH_TO_JSON_4_FILE,
+        PATH_TO_JSON_SECOND_RESULT,
     )
     assert result[0] == result[1]
 
@@ -54,27 +57,15 @@ def test_make_diff_json_2():
 
 def test_make_diff_yaml_1():
 
-    PATH_TO_1_FILE = "tests/fixtures/yaml_1_test_file.yaml"
-    PATH_TO_2_FILE = "tests/fixtures/yaml_2_test_file.yaml"
-    PATH_TO_SECOND_RESULT = "tests/fixtures/yaml_diff_first_result.txt"
-
-    result = check_make_diff(
-        PATH_TO_1_FILE,
-        PATH_TO_2_FILE,
-        PATH_TO_SECOND_RESULT,
-    )
+    result = check_make_diff(PATH_TO_YAML_1_FILE, PATH_TO_YAML_2_FILE, PATH_TO_YAML_FIRST_RESULT)
     assert result[0] == result[1]
 
 
 def test_make_diff_yaml_2():
 
-    PATH_TO_3_FILE = "tests/fixtures/yaml_3_test_file.yaml"
-    PATH_TO_4_FILE = "tests/fixtures/yaml_4_test_file.yaml"
-    PATH_TO_SECOND_RESULT = "tests/fixtures/yaml_diff_second_result.txt"
-
     result = check_make_diff(
-        PATH_TO_3_FILE,
-        PATH_TO_4_FILE,
-        PATH_TO_SECOND_RESULT,
+        PATH_TO_YAML_3_FILE,
+        PATH_TO_YAML_4_FILE,
+        PATH_TO_YAML_SECOND_RESULT,
     )
     assert result[0] == result[1]
