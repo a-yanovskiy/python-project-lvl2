e607fbe14c67711134d59709314a24e142ea41a9
a-yanovskiy <a.e.yanovskiy@gmail.com>
2022-02-08 10:11:03 +0300

    add plain formater

---
+++ gendiff/formaters/plain.py
@@ -0,0 +1,63 @@
+def make_value(diff) -> str:
+    if isinstance(diff, dict):
+        return '[complex value]'
+
+    if diff is None:
+        return "null"
+
+    if isinstance(diff, bool):
+        string = str(diff)
+        return string.lower()
+
+    if isinstance(diff, int):
+        return str(diff)
+
+    if isinstance(diff, str):
+        return f"'{diff}'"
+
+    return diff
+
+
+def get_plain(diff):
+
+    def inner(node, save_key):
+
+        if not isinstance(node, dict):
+            return make_value(node)
+
+        property = ""
+
+        keys = node.keys()
+
+        for key in keys:
+            status = node[key]["status"]
+            body1 = node[key]["body1"]
+
+            if status == "changed":
+
+                save_key += str(key) + '.'
+                property += inner(body1, save_key)
+                if key in save_key.split('.'):
+                    save_key = save_key.replace(f'{key}.', "")
+                else:
+                    save_key = ""
+
+            else:
+                body1 = make_value(body1)
+                if status == 'replaced':
+                    body2 = node[key]["body2"]
+                    body2 = make_value(body2)
+                    property += f"Property '{save_key}{key}' was updated. From {body1} to {body2}\n"
+                
+                elif status == 'added':
+                    property += f"Property '{save_key}{key}' was added with value: {body1}\n"
+
+                elif status == 'deleted':
+                    property += f"Property '{save_key}{key}' was removed\n"
+
+                else:
+                    continue
+            
+        return property
+
+    return inner(diff, save_key="")
--- tests/test_plain.py
+++ tests/test_plain.py
@@ -1,4 +1,4 @@
-from gendiff.formaters.plain import plain
+from gendiff.formaters.plain import get_plain
 import ast
 import pytest
 
@@ -15,7 +15,7 @@ PATH_TO_PLAIN_SECOND_RESULT = "tests/fixtures/plain_second_result.txt"
         (PATH_TO_DIFF_SECOND_RESULT, PATH_TO_PLAIN_SECOND_RESULT),
     ]
 )
-def test_plain(path_to_diff_file, path_to_plained_file):
+def test_get_plain(path_to_diff_file, path_to_plained_file):
 
     with open(path_to_diff_file, 'r') as unformated:
         unformated_file = unformated.read()
@@ -23,6 +23,6 @@ def test_plain(path_to_diff_file, path_to_plained_file):
     with open(path_to_plained_file, 'r') as formated:
         formated_file = formated.read()
 
-    formated_by_plain = plain(unformated_file)
+    formated_by_plain = get_plain(unformated_file)[:-1]
 
-    assert formated_by_plain, formated_file
+    assert formated_by_plain == formated_file
