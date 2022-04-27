6ef62eea5db6f5cfbcb2154e439252a8b882d8cc
a-yanovskiy <a.e.yanovskiy@gmail.com>
2022-04-27 17:39:56 +0300

    fix: json formatter print into command line

--- gendiff/formaters/formaters.py
+++ gendiff/formaters/formaters.py
@@ -1,5 +1,6 @@
 from gendiff.formaters.stylish import get_stylish
 from gendiff.formaters.plain import get_plain
+from gendiff.formaters.json import get_json
 
 
 def formater(diff, formater_str):
@@ -7,6 +8,7 @@ def formater(diff, formater_str):
     formaters = {
         'stylish': get_stylish,
         'plain': get_plain,
+        'json': get_json,
     }
 
     return formaters[formater_str](diff)
