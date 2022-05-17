3051195ba4f8e42493235879c7bccf626c14f417
a-yanovskiy <a.e.yanovskiy@gmail.com>
2022-05-17 15:33:31 +0300

    refactor: rename module

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
-from gendiff.parse_arguments import parse_arguments
-from gendiff.open_files import open_files
-from gendiff.make_diff import make_diff
+from gendiff.cli import parse_arguments
 from gendiff.formatters.formatters import formatter
+from gendiff.make_diff import make_diff
+from gendiff.open_files import open_files
 
 
 def generate_diff(first_file, second_file, format='stylish'):
-
     files = open_files(first_file, second_file)
     file_1 = files[0]
     file_2 = files[1]
