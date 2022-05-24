b7045dfdbe231ec238b062d904a0b40091b7c1fd
a-yanovskiy <a.e.yanovskiy@gmail.com>
2022-05-24 20:08:38 +0300

    Revert "fix!: issue with "unchanged" under "added" or "deleted""
    This reverts commit f94d8b29fc50e9f4f8ebf08552e346d65d49bd45.

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -1,13 +1,13 @@
-from gendiff.check import compare_ext
+from gendiff.check import check_ext
 from gendiff.formatters.formatters import format_diff
 
 
-def make_diff(dict1, dict2):
+def make_diff(dict1, dict2=None):
     if not isinstance(dict1, dict):
         return dict1
 
-    if not isinstance(dict2, dict):
-        return dict2
+    if dict2 is None:
+        dict2 = dict1
 
     keys_set_1 = set(dict1.keys())
     keys_set_2 = set(dict2.keys())
@@ -20,27 +20,21 @@ def make_diff(dict1, dict2):
 
     for key in all_keys:
         if key in added:
-            result[key] = {
-                'type': 'added',
-                'body1': make_diff({}, dict2[key]),
-            }
+            result[key] = {'type': 'added', 'body1': make_diff(dict2[key])}
         elif key in deleted:
-            result[key] = {
-                'type': 'deleted',
-                'body1': make_diff(dict1[key], {}),
-            }
+            result[key] = {'type': 'deleted', 'body1': make_diff(dict1[key])}
         else:
             if dict1[key] == dict2[key]:
                 result[key] = {
                     'type': 'unchanged',
-                    'body1': make_diff(dict1[key], dict2[key])
+                    'body1': make_diff(dict1[key])
                 }
             elif not isinstance(dict1[key], dict) or not isinstance(
                     dict2[key], dict):
                 result[key] = {
                     'type': 'replaced',
-                    'body1': make_diff(dict1[key], {}),
-                    'body2': make_diff({}, dict2[key])
+                    'body1': make_diff(dict1[key]),
+                    'body2': make_diff(dict2[key])
                 }
             else:
                 result[key] = {
@@ -51,7 +45,7 @@ def make_diff(dict1, dict2):
 
 
 def generate_diff(first_file, second_file, format='stylish'):
-    files = compare_ext(first_file, second_file)
+    files = check_ext(first_file, second_file)
     file_1, file_2 = files
 
     diff = make_diff(file_1, file_2)
