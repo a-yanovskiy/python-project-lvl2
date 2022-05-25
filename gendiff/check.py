c6cb24b0b561cc9d14b6ef6221d4a02cb0dd0e8c
a-yanovskiy <a.e.yanovskiy@gmail.com>
2022-05-25 11:22:56 +0300

    refactor: separate parser

--- gendiff/check.py
+++ gendiff/check.py
@@ -1,6 +1,6 @@
 from os import path
 
-from gendiff.load import load_files_by_ext
+from gendiff.load import load_file
 
 
 def compare_ext(first_file, second_file):
@@ -10,4 +10,6 @@ def compare_ext(first_file, second_file):
     if first_file_extension != second_file_extension:
         raise Exception('Files have different formats')
     else:
-        return load_files_by_ext(first_file, second_file, first_file_extension)
+        file_1 = load_file(first_file, first_file_extension)
+        file_2 = load_file(second_file, second_file_extension)
+        return file_1, file_2
--- gendiff/load.py
+++ gendiff/load.py
@@ -3,11 +3,11 @@ import json
 import yaml
 
 
-def load_files_by_ext(file_1, file_2, ext='.json'):
+def load_file(file, ext):
     if ext == '.json':
-        file_1 = json.load(open(file_1))
-        file_2 = json.load(open(file_2))
+        file = json.load(open(file))
     elif ext == '.yaml' or ext == '.yml':
-        file_1 = yaml.safe_load(open(file_1))
-        file_2 = yaml.safe_load(open(file_2))
-    return file_1, file_2
+        file = yaml.safe_load(open(file))
+    else:
+        raise ValueError('Wrong extension!')
+    return file
