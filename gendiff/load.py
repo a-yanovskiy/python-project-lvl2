86a7ad052fc8024f04aa8c6525f292ed4e4aa81a
a-yanovskiy <a.e.yanovskiy@gmail.com>
2022-06-06 21:26:37 +0300

    chore: separate extension checking

--- gendiff/load.py
+++ gendiff/load.py
@@ -4,12 +4,18 @@ from os import path
 import yaml
 
 
-def load_file(filepath):
-    file_extension = path.splitext(filepath)[1]
-    if file_extension == '.json':
-        file = json.load(open(filepath))
-    elif file_extension == '.yaml' or file_extension == '.yml':
-        file = yaml.safe_load(open(filepath))
+def check_extension(filepath):
+    extensions_list = ['.json', '.yaml', '.yml']
+    extension = path.splitext(filepath)[1]
+    if extension in extensions_list:
+        return extension
     else:
         raise ValueError('Wrong extension!')
-    return file
+
+
+def load_file(filepath):
+    file_extension = check_extension(filepath)
+    if file_extension == '.json':
+        return json.load(open(filepath))
+    if file_extension == '.yaml' or file_extension == '.yml':
+        return yaml.safe_load(open(filepath))
