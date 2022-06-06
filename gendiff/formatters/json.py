0d91d58a2ebf3ac1d4ea0691edd38b9d5ac6af8b
a-yanovskiy <a.e.yanovskiy@gmail.com>
2022-06-06 21:02:24 +0300

    refactor: del unusible code

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -2,12 +2,4 @@ import json
 
 
 def get_json(diff):
-    str_diff = str(diff)
-    replaced_quotes = str_diff.replace("\'", "\"")
-    replaced_bools = (
-        replaced_quotes.replace("True", "true")).replace("False", "false")
-    replaced_nulls = replaced_bools.replace("None", "null")
-
-    json_loads = json.loads(replaced_nulls)
-    json_dumps = json.dumps(json_loads, indent=2, sort_keys=True)
-    return json_dumps
+    return json.dumps(diff, indent=2, sort_keys=True)
